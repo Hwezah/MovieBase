c02ff026b506e3916b0a4afb3b98531ea3594719
Hwezah <hwezah.dev@gmail.com>
2026-03-07 00:13:22 +0300

    minor padding fix

--- components/providerRows/tmdbProvider.js
+++ components/providerRows/tmdbProvider.js
@@ -9,7 +9,7 @@ export default async function TmdbProvider({ category, type }) {
       {movies.map((movie) =>
         movie.poster_path ? (
           <Link href={`/movie/${movie.id}`} key={movie.id}>
-            <div className="relative cursor-pointer flex-shrink-0 w-48 h-72 rounded-md snap-start hover:scale-105 transition-transform shadow-lg bg-gray-800">
+            <div className="relative cursor-pointer flex-shrink-0 w-28 h-42 md:w-36 md:h-52 rounded-md snap-start hover:scale-105 transition-transform shadow-lg bg-gray-800">
               <Image
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
@@ -17,9 +17,16 @@ export default async function TmdbProvider({ category, type }) {
                 className="object-cover rounded-md"
               />
               <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-2">
-                <h3 className="text-lg font-bold text-white">{movie.title}</h3>
-                <p className="text-gray-300">{movie.release_date}</p>
+                <h3 className="text-lg font-semibold text-white text-sm md:text-base lg:text-lg xl:text-xl">
+                  {movie.title}
+                </h3>
+                <p className="text-gray-300 text-sm md:text-base lg:text-lg xl:text-xl">
+                  {movie.release_date}
+                </p>
               </div>
+              <p className="text-gray-300 text-sm md:text-base lg:text-lg xl:text-xl absolute top-2 right-2 bg-black bg-opacity-75 text-yellow-500 px-1 rounded">
+                {movie.vote_average.toFixed(1)}
+              </p>
             </div>
           </Link>
         ) : null,
