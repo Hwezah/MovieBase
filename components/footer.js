67a4bd22d91313c622d34b55218c6535545597bc
Hwezah <hwezah.dev@gmail.com>
2026-03-09 23:13:35 +0300

    improved fixed footer

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import { Home, Search, Folder, Download, Settings } from "lucide-react";
 import Link from "next/link";
 export default function Footer() {
   return (
-    <footer className="text-gray-400 py-6 text-center">
+    <footer className="text-gray-400 py-6 text-center fixed bottom-0 left-0 right-0 bg-gray-800 bg-opacity-90 backdrop-blur-sm z-10">
       <div className="flex items-center space-x-4 justify-around">
         <Link href="/">
           <Home />
