a24ab917c2be5de747823ce7dbf68058d4b16057
Hwezah <hwezah.dev@gmail.com>
2026-03-19 07:15:59 +0300

    added PWA Setup to help turn the WebApp into an Android App

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,25 @@
+import withPWA from "@ducanh2912/next-pwa"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "image.tmdb.org",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig)
